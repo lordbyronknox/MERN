cc5a30e91be7ff33ed9eb1572d6eab7db75386c3
lordbyron <byronknox@yahoo.co.uk>
2022-06-01 13:25:18 +0200

    Netflix - reCloned admin file

--- netflix/admin/src/index.js
+++ netflix/admin/src/index.js
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { AuthContextProvider } from "./context/authContext/AuthContext";
-import { MovieContextProvider } from "./context/movieContext/MovieContext";
-import { ListContextProvider } from "./context/listContext/ListContext";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <MovieContextProvider>
-        <ListContextProvider>
-          <App />
-        </ListContextProvider>
-      </MovieContextProvider>
-    </AuthContextProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
