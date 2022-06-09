715a5a30b1086e6097983fde661b43b7e06db76b
lordbyron <byronknox@yahoo.co.uk>
2022-06-09 12:29:12 +0200

    Dynamic table completed

--- dynamic-table/src/dynamic-table/TableData.js
+++
@@ -1,9 +0,0 @@
-const TableData = [
-    {id: 1, fullname: "Noor Khan", age: 25, city: "Patna"},
-    {id: 2, fullname: "Rapsan Jani", age: 26, city: "Noida"},
-    {id: 3, fullname: "Monika Singh", age: 18, city: "New Dehli"},
-    {id: 4, fullname: "Sunil Kumar", age: 22, city: "Jaipur"},
-    {id: 5, fullname: "Kajol Kumari", age: 21, city: "Chennai"}
-]
-
-export default TableData;
