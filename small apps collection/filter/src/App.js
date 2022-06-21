7b7917c2ca0fa8bc3f1868cf87eaacd20dfe69f0
lordbyron <byronknox@yahoo.co.uk>
2022-06-21 13:06:38 +0200

    filter complete

--- small apps collection/filter/src/App.js
+++ small apps collection/filter/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import JSONDATA from './MOCK_DATA.json';
+import { useState } from 'react';
 
 function App() {
+const [searchTerm, setsearchTerm] = useState('');
+
   return (
     <div className="App">
-      
+      <input 
+      type="text" 
+      placeholder="Search..." 
+      onChange={(event) => {
+        setsearchTerm(event.target.value);
+      }} 
+      />
+      {JSONDATA.filter((val) => {
+        if (searchTerm == "") {
+          return val
+        } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val
+        }
+      }).map((val, key) => {
+       return (
+       <div className='user' key={key}> 
+        <p>{val.id} - {val.first_name} {val.last_name} </p>
+        </div>
+       );
+      })}
     </div>
   );
 }
